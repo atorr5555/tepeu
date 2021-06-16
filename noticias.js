19bb918ddce0efa2ae0fe0fd5be6ee4e857bc995
atorr5555 <31078782+atorr5555@users.noreply.github.com>
2021-06-16 14:53:06 -0500

    Avance slider noticias

---
+++ noticias.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        autoHeight:true,
+        items: 1,
+        nav:true,
+        navText : ['<i class="fa fa-angle-left" aria-hidden="true"></i>','<i class="fa fa-angle-right" aria-hidden="true"></i>'],
+        autoplay:true,
+        smartSpeed:2000,
+        autoplayHoverPause:true
+    })
+  });
+
+  
