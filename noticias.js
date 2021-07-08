d5f871833302c3a29763c3daf9809aaf468f22bd
atorr5555 <31078782+atorr5555@users.noreply.github.com>
2021-07-08 00:52:42 -0500

    Correccion Noticias

--- noticias.js
+++ noticias.js
@@ -4,8 +4,8 @@ $(document).ready(function(){
         margin:10,
         autoHeight:true,
         items: 1,
-        nav:true,
-        navText : ['<i class="fa fa-angle-left" aria-hidden="true"></i>','<i class="fa fa-angle-right" aria-hidden="true"></i>'],
+        nav:false,
+        //navText : ['<i class="fa fa-angle-left" aria-hidden="true"></i>','<i class="fa fa-angle-right" aria-hidden="true"></i>'],
         autoplay:true,
         smartSpeed:2000,
         autoplayHoverPause:true
